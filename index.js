2a6bf65082df3a9bb9471583031871e110889d8c
Chariey-01 <charityjepkoech007@gmail.com>
2026-04-05 03:01:28 +0300

    Added code for scope lab test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Global variables attached to window
+window.burgers = ["Hamburger", "Cheeseburger"];
+window.featuredDrink = "Strawberry Milkshake";
+
+// Function-scoped variable
+function addBurger() {
+  const newBurger = "Flatburger";
+  window.burgers.push(newBurger);
+}
+
+// Block-scoped variable
+if (true) {
+  const anotherNewBurger = "Maple Bacon Burger";
+  window.burgers.push(anotherNewBurger);
+}
+
+// Function to change the featured drink
+function changeFeaturedDrink() {
+  window.featuredDrink = "The JavaShake";
+}
