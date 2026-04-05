c3ce5486357c0426685e03f413dba7ee328bcd09
Chariey-01 <charityjepkoech007@gmail.com>
2026-04-05 03:08:01 +0300

    Changed window. to variables since code grade penelized me for that

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// Global variables attached to window
-window.burgers = ["Hamburger", "Cheeseburger"];
-window.featuredDrink = "Strawberry Milkshake";
+// Global variables (declared in global scope)
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
 
 // Function-scoped variable
 function addBurger() {
+  // newBurger is function-scoped to addBurger
   const newBurger = "Flatburger";
-  window.burgers.push(newBurger);
+  burgers.push(newBurger);
 }
 
 // Block-scoped variable
 if (true) {
+  // anotherNewBurger is block-scoped inside this if statement
   const anotherNewBurger = "Maple Bacon Burger";
-  window.burgers.push(anotherNewBurger);
+  burgers.push(anotherNewBurger);
 }
 
 // Function to change the featured drink
 function changeFeaturedDrink() {
-  window.featuredDrink = "The JavaShake";
+  // Reassign featuredDrink
+  featuredDrink = "The JavaShake";
 }
