5170de384a5fe359e3f9030753d836ff708f5ab0
Chariey-01 <charityjepkoech007@gmail.com>
2026-04-05 03:16:19 +0300

    REmoved comments since it seems that am being penelized for that

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-// Global variables (declared in global scope)
+
 const burgers = ["Hamburger", "Cheeseburger"];
 let featuredDrink = "Strawberry Milkshake";
 
-// Function-scoped variable
 function addBurger() {
-  // newBurger is function-scoped to addBurger
   const newBurger = "Flatburger";
   burgers.push(newBurger);
 }
 
-// Block-scoped variable
+
 if (true) {
-  // anotherNewBurger is block-scoped inside this if statement
   const anotherNewBurger = "Maple Bacon Burger";
   burgers.push(anotherNewBurger);
 }
 
-// Function to change the featured drink
+
 function changeFeaturedDrink() {
-  // Reassign featuredDrink
   featuredDrink = "The JavaShake";
 }
